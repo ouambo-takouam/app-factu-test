7cc804abf2771bb81550751fa7e62162b0a48a2b
patrick ouambo <patrickouambo@gmail.com>
2023-05-19 19:06:12 +0200

    customer details added

--- client/src/pages/customers/customer-details/customer-details.component.jsx
+++ client/src/pages/customers/customer-details/customer-details.component.jsx
@@ -1,30 +1,45 @@
 import { IoIosArrowBack } from 'react-icons/io';
 import { RxPlus } from 'react-icons/rx';
 import { RxHamburgerMenu } from 'react-icons/rx';
+import { Link, useNavigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 import InputField from '../../../components/form/input-field/input-field.component';
 import './customer-details.styles.scss';
 
 export default function CustomerDetails() {
+	const navigate = useNavigate();
+	const customers = useSelector((state) => state.data.customers);
+
 	return (
 		<div className="customers-details">
 			<div className="left-bar-wrapper">
 				<div className="left-bar-header">
 					<div className="row">
-						<div className="back-to-clients">
-							<IoIosArrowBack />
-							<span>Clients</span>
+						<div
+							className="back-to-clients"
+							onClick={() => navigate('/app/clients')}
+						>
+							<span className="arrow">
+								<IoIosArrowBack size={20} />
+							</span>
+							<Link to="/app/clients">Clients</Link>
 						</div>
 						<div className="options">
-							<RxPlus />
-							<RxHamburgerMenu />
+							<RxPlus size={17} />
+							<RxHamburgerMenu size={17} />
 						</div>
 					</div>
 					<div className="filter">
-						<InputField fullwidth />
+						<InputField fullwidth placeholder="Filtrer par nom" />
 					</div>
 				</div>
 				<div className="left-bar-content">
-					Liste de clients dans la base de donnees
+					{customers.map((customer) => (
+						<div className="customer-preview" key={customer._id}>
+							<div className="display-name">{customer.display_name}</div>
+							<div className="balance">XAF0.00</div>
+						</div>
+					))}
 				</div>
 			</div>
 			<div className="customer-details-content">
